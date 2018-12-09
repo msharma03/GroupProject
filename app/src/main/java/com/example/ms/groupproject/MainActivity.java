65ca77e5f0f193147510e814709ead73f5322173
mjcorley <mjcorley@umich.edu>
2018-12-09 16:54:57 -0500

    initial commit

--- app/src/main/java/com/example/ms/groupproject/MainActivity.java
+++ app/src/main/java/com/example/ms/groupproject/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.ms.groupproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button buttonRegister, buttonLogin;
+    EditText editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonRegister = findViewById(R.id.buttonRegister);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        buttonLogin.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View v) {
+        final String email = editTextEmail.getText().toString();
+        if (email.equals(null)  || email.equals("")) {
+            return;
+        }
+        String password = editTextPassword.getText().toString();
+        if (password.equals(null)  || password.equals("")) {
+            return;
+        }
+        if(v == buttonRegister) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(MainActivity.this, "Welcome" + email, Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(MainActivity.this, "Access Denied", Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+        } else if(v == buttonLogin) {
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(MainActivity.this,"Login Successful: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                                Intent intentHome = new Intent(MainActivity.this, HomeActivity.class);
+                                startActivity(intentHome);
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(MainActivity.this, "Login Denied", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+
+        }
     }
 }
