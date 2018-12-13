fd92d9b28327e88aa333ecadf55d28943c26a1d8
malvika <malvika.sh@gmail.com>
2018-12-13 15:08:34 -0500

    Hotels activity

--- app/src/main/java/com/example/ms/groupproject/HotelActivity.java
+++ app/src/main/java/com/example/ms/groupproject/HotelActivity.java
@@ -1,19 +1,45 @@
 package com.example.ms.groupproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HotelActivity extends Activity implements View.OnClickListener {
+
+    Button buttonViewHotel;
+    EditText editTextHotelDay;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
+        mAuth = FirebaseAuth.getInstance();
+
+        buttonViewHotel = findViewById(R.id.buttonViewHotel);
+        editTextHotelDay = findViewById(R.id.editTextHotelDay);
+
+        buttonViewHotel.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        //myRef.setValue("");
+
+        if (v == buttonViewHotel)
+        {
+            String getDay = editTextHotelDay.getText().toString();
 
     }
 }
--- app/src/main/java/com/example/ms/groupproject/Hotels.java
+++ app/src/main/java/com/example/ms/groupproject/Hotels.java
@@ -1,4 +1,13 @@
 package com.example.ms.groupproject;
 
 public class Hotels {
+    String hotelname, address, link;
+    int day;
+    public Hotels(String hotelname, String address, String link){
+        this.hotelname = hotelname;
+        this.address = address;
+        this.link = link;
+    }
+        public Hotels() {
+                }
 }
